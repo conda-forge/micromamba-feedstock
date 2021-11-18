dda4c8d040787bd69650f09447e93fd00b72f513
Adrien DELSALLE <ad.delsalle@gmail.com>
2021-11-18 13:26:39 +0100

    release 0.18.0 add spdlog and termcolor-cpp deps add licenses update build scripts

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,11 +4,10 @@ cd build
 cmake ${CMAKE_ARGS} .. \
          -DCMAKE_INSTALL_PREFIX=${PREFIX} \
          -DCMAKE_BUILD_TYPE="Release" \
-         -DBUILD_EXE=ON \
-         -DBUILD_BINDINGS=OFF \
-         -DBUILD_STATIC=ON \
-         -DBUILD_SHARED=OFF \
-         -DSTATIC_DEPENDENCIES=ON
+         -DBUILD_LIBMAMBA=ON \
+         -DBUILD_STATIC_DEPS=ON \
+         -DBUILD_MICROMAMBA=ON \
+         -DMICROMAMBA_LINKAGE=FULL_STATIC
 
 make -j${CPU_COUNT}
 make install
